8230a133cf3c4debaf0b199e878f09a5390b2de9
meli-mac <melissamachado015@gmail.com>
2024-01-03 10:52:50 -0500

    Added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateJoke(event) {
+  event.preventDefault();
+  new Typewriter("#joke", {
+    strings: "The joke will go here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
+let jokeFormElement = document.querySelector("#joke-generator-form");
+jokeFormElement.addEventListener("submit", generateJoke);
