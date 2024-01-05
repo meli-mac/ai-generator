cb89f06163e81707853e814c1793851fa2c97e81
meli-mac <melissamachado015@gmail.com>
2024-01-05 10:18:38 -0500

    Improved user experience
    - Added a hint under the input

--- src/index.js
+++ src/index.js
@@ -14,8 +14,11 @@ function generateJoke(event) {
   let apiKey = "560eatef1f75aofbc96fd19f393b84ab";
   let prompt = `User instructions: Generate a joke about ${instructionsInput.value}`;
   let context =
-    "You are a funny joke expert and love to tell funny jokes. Your mission is to generate a joke in basic HTMl and separate each line with a <br />. Do not include a title to the joke. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the joke and NOT at the beginning. Make sure to follow the user intructions.";
+    "You are a funny joke expert and love to tell funny jokes. Your mission is to generate a joke in basic HTMl and separate each line with a <br />. Do not include a title to the joke. Only include one joke. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the joke and NOT at the beginning. Make sure to follow the user intructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let jokeElement = document.querySelector("#joke");
+  jokeElement.classList.remove("hidden-display");
+  jokeElement.innerHTML = `Generating the joke about ${instructionsInput.value} <span class="blink">⏳</span>`;
   axios.get(apiUrl).then(displayJoke);
   console.log("Generating joke");
   console.log(`Prompt: ${prompt}`);
